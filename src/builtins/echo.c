0d23d8b14c9aee5abc886c741ab4408de3228902
Aingeru <aingeru.alvarez@outlook.com>
2022-06-09 14:50:20 +0200

    echo works

--- src/builtins/echo.c
+++ src/builtins/echo.c
@@ -6,26 +6,27 @@
 /*   By: aalvarez <aalvarez@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/07 12:47:14 by aalvarez          #+#    #+#             */
-/*   Updated: 2022/06/08 21:57:08 by aalvarez         ###   ########.fr       */
+/*   Updated: 2022/06/09 14:47:17 by aalvarez         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../include/minishell.h"
 
-static void	ft_exec_echo(t_command *command, int start, int flag)
+static void	ft_exec_echo(t_command *command, int start)
 {
 	int	x;
 
-	while (command->command[++start])
+	if (command->command[start])
 	{
-		x = -1;
-		while (command->command[start][++x])
-			write(1, &command->command[start][x], 1);
-		if (command->command[start + 1])
-			write(1, " ", 1);
+		while (command->command[++start])
+		{
+			x = -1;
+			while (command->command[start][++x])
+				write(1, &command->command[start][x], 1);
+			if (command->command[start + 1])
+				write(1, " ", 1);
+		}
 	}
-	if (flag == 1)
-		write(1, "\n", 1);
 }
 
 void	ft_echo(t_command *command)
@@ -33,16 +34,20 @@ void	ft_echo(t_command *command)
 	int	i;
 
 	i = 1;
-	if (!ft_strncmp(command->command[1], "-n", 2))
+	if (command->command[1] && !ft_strncmp(command->command[1], "-n", 2))
 	{
 		while (command->command[1][++i])
 		{
 			if (command->command[1][i] != 'n')
 			{
-				ft_exec_echo(command, 0, 1);
+				ft_exec_echo(command, 0);
+				write(1, "\n", 1);
 				return ;
 			}
 		}
+		ft_exec_echo(command, 1);
+		return ;
 	}
-	ft_exec_echo(command, 1, 0);
+	ft_exec_echo(command, 0);
+	write(1, "\n", 1);
 }
