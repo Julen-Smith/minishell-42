98520e9cc8427e0541dc5c051a260e51ff861abb
Aingeru <aingeru.alvarez@outlook.com>
2022-07-13 23:39:02 +0200

    double quotes fixed, new error case

--- README.md
+++ README.md
@@ -23,12 +23,13 @@
 			To upper to lower. ⎷
 
 	Casos de error :
-		Trimear comillas para comandos "ls" -l
-		Echo trim
+		Trimear comillas para comandos "ls" -l⎷
+		corregir trimeo de casos como echo "'"hola"'" -> 'hola'
+		Echo trim ⎷
 		UNSET revision  además de mirar por que no lo pilla en upper
-		EXIT si tienes mas de un argumento error
-			-Si tiene un argumento sale 
-		EXPANSION DE DOLAR $USE"R" debe dar > Rº
+		EXIT si tienes mas de un argumento error ⎷
+			-Si tiene un argumento sale ⎷
+		EXPANSION DE DOLAR $USE"R" debe dar > R 
 		Comprobar que entre pipes haya cosas
 		Comprobar que antes de cualquier comando no solo exista una pipe
 		Comprobacion cat y wc
--- src/execution/binary_manage.c
+++ src/execution/binary_manage.c
@@ -6,7 +6,7 @@
 /*   By: aalvarez <aalvarez@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/09 13:16:49 by jsmith            #+#    #+#             */
-/*   Updated: 2022/07/13 00:58:55 by aalvarez         ###   ########.fr       */
+/*   Updated: 2022/07/13 23:37:08 by aalvarez         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -87,7 +87,7 @@ char	*reach_bin_path(t_command *command, t_msh_var *msh)
 	string_to_lower(command->command[0]);
 	while (command->command[++i])
 	{
-		if (command->command[i][ft_strlen(command->command[i])] == '"')
+		if (command->command[i][ft_strlen(command->command[i]) - 1] == '"')
 		{
 			tmp = ft_strtrim(command->command[i], "\"");
 			free(command->command[i]);
