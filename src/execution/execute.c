70456cf078b1aa8aaacfb23234ca1e9d4acb91a9
AingeruAlvarezSanchez <aingeru.alvarez@outlook.com>
2022-07-16 13:45:45 -0700

    execution fixed

--- src/execution/execute.c
+++ src/execution/execute.c
@@ -6,7 +6,7 @@
 /*   By: aalvarez <aalvarez@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/09 07:41:25 by jsmith            #+#    #+#             */
-/*   Updated: 2022/07/16 02:57:11 by aalvarez         ###   ########.fr       */
+/*   Updated: 2022/07/16 13:45:13 by aalvarez         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -57,10 +57,10 @@ void	father_fd_closes(t_command_table *table)
 
 void	ft_childexec(t_msh_var *msh, t_command_table *table, int i)
 {
-	if (!ft_child_builtin(&table->commands[i], msh))
-		exit(0);
 	if (table->cmd_count > 1)
 		dup_son_choose(i, table);
+	if (!ft_child_builtin(&table->commands[i], msh))
+		exit(0);
 	if (table->commands[i].is_absolute)
 		execve(table->commands[i].bin_path,
 			table->commands[i].command, msh->own_envp);
